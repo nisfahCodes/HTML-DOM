963ba3ebe2c5bcee7142e62dd15f520c8a17e5bd
Nisfah <nisfahanjum@gmail.com>
2026-01-03 09:54:41 +0200

    form

---
+++ script1.js
@@ -0,0 +1,25 @@
+function validate(e) {
+    e.preventDefault();
+
+const email = document.getElementById('email').value
+const pass = document.getElementById('password').value;
+const age = document.getElementById('age').value;
+const msgBox = document.getElementById('message');
+
+let message = '';
+
+if (email ==='') {
+    message = 'enter an email.';
+    msgBox.style.color = 'red';
+} else if (pass === '') {
+     message = 'enter an email.';
+    msgBox.style.color = 'red';
+    } else if (age === '') {
+     message = 'enter an email.';
+    msgBox.style.color = 'red';
+    } else {
+     message = 'Login successful';
+    msgBox.style.color = 'green';    
+    };
+    msgBox.innerText =message;
+}
